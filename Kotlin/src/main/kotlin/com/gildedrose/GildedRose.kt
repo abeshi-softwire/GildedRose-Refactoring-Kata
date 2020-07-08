69bb786f705577ff7c8b61f0e7035ef9bd480efe
Olly Larkin <olly.larkin@softwire.com>
2020-07-08 10:58:04 +0100

    further refactoring

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,59 +5,61 @@ import kotlin.math.min
 
 class GildedRose(var items: Array<Item>) {
 
-    val MIN_QUALITY = 0
-    val MAX_QUALITY = 50
-    val AGED_BRIE_NAME = "Aged Brie"
-    val SULFURAS_NAME = "Sulfuras, Hand of Ragnaros"
-    val BACKSTAGE_PASS_NAME = "Backstage passes to a TAFKAL80ETC concert"
-    val CONJURED_NAME = "Conjured "
+    private val MIN_QUALITY = 0
+    private val MAX_QUALITY = 50
+    private val AGED_BRIE_NAME = "Aged Brie"
+    private val SULFURAS_NAME = "Sulfuras, Hand of Ragnaros"
+    private val BACKSTAGE_PASS_NAME = "Backstage passes to a TAFKAL80ETC concert"
+    private val CONJURED_NAME = "Conjured "
 
-    private fun addQuality(item : Item, con: Boolean, quality: Int) {
+    private fun addQuality(item : Item, isConjured: Boolean, qualityChange: Int) {
         // Adds quality to item, then ensures MIN_QUALITY <= item.quality <= MAX_QUALITY
-        val updatedQuality = if (con && quality < 0) quality * 2 else quality
-        item.quality = max(min(item.quality + updatedQuality, MAX_QUALITY), MIN_QUALITY)
+        // QualityChange is doubled if it is conjured and quality is decreasing
+        // Also doubled if it is past the sell by date
+        val updatedQualityChange = qualityChange * (if (isConjured && qualityChange < 0) 2 else 1) * (if (item.sellIn < 0) 2 else 1)
+        item.quality = max(min(item.quality + updatedQualityChange, MAX_QUALITY), MIN_QUALITY)
+    }
+
+    data class Name(val itemName: String, val isConjured: Boolean)
+
+    private fun getConjuredAndName(name: String) : Name {
+        val isConjured: Boolean
+        val itemName: String
+        if (name.startsWith(CONJURED_NAME)) {
+            isConjured = true
+            itemName = name.substring(CONJURED_NAME.length)
+        } else {
+            isConjured = false
+            itemName = name
+        }
+        return Name(itemName, isConjured)
     }
 
     fun updateQuality() {
         for (item in items) {
 
-            val con: Boolean
-            val name: String
-            if (item.name.startsWith(CONJURED_NAME)) {
-                con = true
-                name = item.name.substring(CONJURED_NAME.length)
-            } else {
-                con = false
-                name = item.name
-            }
+            val (itemName, isConjured) = getConjuredAndName(item.name)
 
-            when (name) {
-                AGED_BRIE_NAME -> addQuality(item, con, 1)
-                BACKSTAGE_PASS_NAME ->
-                    when (item.sellIn) {
-                        in Int.MIN_VALUE..0 -> item.quality = 0
-                        in 1..5 -> addQuality(item, con, 3)
-                        in 6..10 -> addQuality(item, con, 2)
-                        else -> addQuality(item, con, 1)
-                    }
-                SULFURAS_NAME -> {}
-                else -> addQuality(item, con, -1)
-            }
+            // Sulfuruas is never changed and should be skipped
+            if (itemName != SULFURAS_NAME) {
+                item.sellIn -= 1
 
-            if (name != SULFURAS_NAME) {
-                item.sellIn = item.sellIn - 1
-            }
-
-            if (item.sellIn < 0) {
-                when (name) {
-                    // According to the legacy code, aged brie increases by 2 after sellIn has passed.
-                    AGED_BRIE_NAME -> addQuality(item, con, 1)
-                    SULFURAS_NAME -> {}
-                    else -> addQuality(item, con, -1)
+                when (itemName) {
+                    AGED_BRIE_NAME -> addQuality(item, isConjured, 1)
+                    BACKSTAGE_PASS_NAME ->
+                        // The legacy code functionality has been mimicked here
+                        // Check the range of sellIn before it has been decremented and update quality accordingly
+                        // The range has been adjusted to compensate for sellIn being decremented before
+                        when (item.sellIn) {
+                            in Int.MIN_VALUE..0 -> item.quality = 0
+                            in 0..4 -> addQuality(item, isConjured, 3)
+                            in 5..9 -> addQuality(item, isConjured, 2)
+                            else -> addQuality(item, isConjured, 1)
+                        }
+                    else -> addQuality(item, isConjured, -1)
                 }
             }
         }
     }
-
 }
 
